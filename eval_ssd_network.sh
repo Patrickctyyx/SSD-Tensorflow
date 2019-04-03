c90f461a157759da1f4dc83abb1334f2f94e5175
patrickcty <chengtiyanyang@gmail.com>
2019-04-03 11:08:51 +0800

    Revise parameters according to new environment CUDA 10.0

--- eval_ssd_network.sh
+++ eval_ssd_network.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_HOME=/usr/local/cuda
 DATASET_DIR=./tfrecords/test
 TRAIN_DIR=./logs/
@@ -14,4 +14,5 @@ nohup python3 -u eval_ssd_network.py \
     --checkpoint_path=${TRAIN_DIR} \
     --wait_for_checkpoints=True \
     --batch_size=1 \
+    --num_classes=8 \
     --max_num_batches=500 > ./logs/ssd_eval_log 2>&1 &
--- fine_tune_ssd_network.sh
+++ fine_tune_ssd_network.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_HOME=/usr/local/cuda
 DATASET_DIR=./tfrecords/train
 TRAIN_DIR=./logs/
